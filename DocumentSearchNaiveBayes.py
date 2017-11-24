f48bf905c8f9f4e68d3a99d7f8ee22cf13c3f902
satoshi5518 <ohi_stoshi@yahoo.co.jp>
2017-11-25 00:59:42 +0900

    aaa

--- DocumentSearchNaiveBayes.py
+++ DocumentSearchNaiveBayes.py
@@ -25,4 +25,26 @@ for category in nb.categories:
 for category in nb.categories:
     test = fc.importTestFile(r'.\TestData\餃子_TEST0001.txt')
     print("log P(" + category + "|test) = ",nb.score(test,category))
+print(nb.classify(test))
 
+# data = [["yes", "Chinese", "Beijing", "Chinese"],
+#         ["yes", "Chinese", "Chinese", "Shanghai"],
+#         ["yes", "Chinese", "Macao"],
+#         ["no", "Tokyo", "Japan", "Chinese"]]
+
+# # ナイーブベイズ分類器を訓練
+# nb = NaiveBayes()
+# nb.train(data)
+
+# print("P(Chinese|yes) = ", nb.word_probability("Chinese", "yes"))
+# print("P(Tokyo|yes) = ", nb.word_probability("Tokyo", "yes"))
+# print("P(Japan|yes) = ", nb.word_probability("Japan", "yes"))
+# print("P(Chinese|no) = ", nb.word_probability("Chinese", "no"))
+# print("P(Tokyo|no) = ", nb.word_probability("Tokyo", "no"))
+# print("P(Japan|no) = ", nb.word_probability("Japan", "no"))
+# #
+# # # テストデータのカテゴリを予測
+# test = ["Tokyo", "Japan"]
+# print("log P(yes|test) =", nb.score(test, "yes"))
+# print("log P(no|test) =", nb.score(test, "no"))
+# print(nb.classify(test))
