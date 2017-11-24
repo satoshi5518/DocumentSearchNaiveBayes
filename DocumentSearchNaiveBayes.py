2e2a0e928ddade599f860e9568209bd761afeb2c
satoshi5518 <ohi_stoshi@yahoo.co.jp>
2017-11-24 23:56:18 +0900

    file import

--- DocumentSearchNaiveBayes.py
+++ DocumentSearchNaiveBayes.py
@@ -7,3 +7,7 @@ print('メイン処理開始')
 fc = FileController()
 fc.importTrainingFile(r'.\TrainingData\*_*.txt')
 
+for traingData in fc.trainingDataList:
+    for data in traingData:
+        print(data)
+
--- FileController.py
+++ FileController.py
@@ -8,13 +8,22 @@ class FileController:
     # 初期処理
     def __init__(self) :
         print("初期処理")
+        self.trainingDataList = [[]]
 
     def importTrainingFile(self,path = "") :
         files = glob.glob(path)
 
         for file in files:
-            print(file)
-            tag = re.match(r'\.*_',file)
-            print(tag)
-            # for line in open(file,'r',encoding="utf-8"):
+
+            trainingData = []
+            # print(file)
+            tag = re.search(r'_.*?_',file).group().replace('_','')
+            # print(tag)
+            trainingData.append(tag)
+            
+            for line in open(file,'r',encoding="utf-8"):
                 # print(line)
+                # self.trainingData.append(line)
+                trainingData.append(line)
+            
+            self.trainingDataList.append(trainingData)
